e4775253154d4f99ab7e9571ba1e52d5271ad7b3
Leo Postovoit <musicman5821@gmail.com>
2018-03-03 14:34:37 -0800

    Update mackbprofile.sh

--- mackbprofile.sh
+++ mackbprofile.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# chmod +x to this file, prevent loading by dash by running script this way:
+# ./mackbprofile.sh
+
 # trap ctrl-c and call ctrl_c()
 trap ctrl_c INT
 
 function ctrl_c() {
         setxkbmap -option
+        echo 'Default KB Loaded'
 }
 
-# Run Xmodmap repeatedly using watch
+# Run Xmodmap repeatedly using watch every 10 seconds. Useful if it falls asleep.
 watch -n 10 "xmodmap -pke ~/.Xmodmap_mac"
